d0f6ff931c2279358546f75ba987a2bf99bb3871
Ricardo <ricardo_costa20@hotmail.com>
2020-06-17 14:50:42 +0100

    criar node express

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
-
+//sdjfksdf
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
